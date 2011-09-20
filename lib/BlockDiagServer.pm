d7d62c908c386078af724cb4a212d71ac4243624
Matsuki Masayuki <matsuki-masayuki@kayac.com>
2011-09-20 20:59:36 +0900

    無理やり

---
+++ lib/BlockDiagServer.pm
@@ -0,0 +1,28 @@
+package BlockDiagServer;
+use strict;
+use warnings;
+use utf8;
+use IPC::Run qw/run/;
+use File::Temp qw/tempfile/;
+
+sub render {
+    my $blockdiag = shift;
+    local $| = 1;
+    utf8::encode($blockdiag);
+    my ($fh, $filename) = tempfile;
+    $fh->print($blockdiag);
+    $fh->close;
+
+    my $err;
+    run ['blockdiag', '-a', $filename], undef, undef, \$err or die $err;
+
+    my $png_file = $filename .'.png';
+    do {
+        local $/;
+        open my $fh, '<:raw', $png_file;
+        <$fh>
+    };
+
+}
+
+1;
